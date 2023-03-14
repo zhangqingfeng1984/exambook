f99fe3970b86b2252b53764d5c35322653b5aeb7
zhangqingfeng <zhangqingfeng@qq.com>
2023-03-14 20:42:07 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
+import {Breadcrumbs, Link, Typography} from "@mui/material"
 
 function App() {
   return (
     <div>
-      <Header/>
-      <div style={{height: 600}}>lorem</div>
-      <Footer/>
+      <Header />
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          Home
+        </Link>
+        <Typography color="text.primary">Practice test</Typography>
+      </Breadcrumbs>
+      <div style={{ height: 600 }}>lorem</div>
+      <Footer />
     </div>
   );
 }
